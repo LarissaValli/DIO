a54549f345807265b2dc6f9c205e7052665ea801
LarissaValli <larissavalli4@gmail.com>
2025-07-22 15:20:10 -0300

    aprendendo c#

--- models/Pessoa.cs
+++ models/Pessoa.cs
@@ -8,5 +8,11 @@ namespace DIO.models
     public class Pessoa
     {
         public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public void Apresentar()
+        {
+            console.WriteLine($"Olá, meu nome é {Nome}, e tenho {idade} anos;")
+        }
     }
 }

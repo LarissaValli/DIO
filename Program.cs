f66e28f3b0f64c14ee3dcb9585172953fe49773b
LarissaValli <larissavalli4@gmail.com>
2025-07-23 16:29:52 -0300

    operador condicional na pratica

--- Program.cs
+++ Program.cs
@@ -3,10 +3,14 @@
 //OPERADORES CONDICIONAIS
 
 int quantidadeEmEstoque = 10;
-
 int quantidadeCompra = 4;
+bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
 
-if (quantidadeEmEstoque >= quantidadeCompra)
+Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
+Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
+
+if (possivelVenda)
 {
     Console.WriteLine("Venda realizada.");
 }
